2ff261539e00f8f66854800135ceebe26e657f21
Slayer <slayer@vbox.home>
2025-11-07 00:51:29 +0000

    started to try to implement the recursive backtraking to check if is possible to exit the map

--- parcer_semantics.c
+++ parcer_semantics.c
@@ -6,7 +6,7 @@
 /*   By: slayer <slayer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 09:50:32 by slayer            #+#    #+#             */
-/*   Updated: 2025/11/06 20:33:35 by slayer           ###   ########.fr       */
+/*   Updated: 2025/11/07 00:49:21 by slayer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,25 @@ int	check_walls_top_bootom(t_Level *level)
 	return (0);
 }
 
-int	is_exit_posible(t_Level *level)
+int	is_exit_posible(t_Pos *last_pos, int x, int y, t_Level *level)
 {
-	int		i;
-	int		j;
+	char	this_pos_content;
+	t_Pos	*this_pos;
 
-	
+	this_pos->x = last_pos->x + x;
+	this_pos->y = last_pos->y + y;
+	this_pos_content = level->map[this_pos->x][this_pos->y];
+
+	if (this_pos_content == '1')
+		return (is_exit_posible(last_pos, x, y, level));
+	if (this_pos_content == 'E')
+		return (1);
+	return (is_exit_posible(this_pos, x, y, level));
 }
 
 int	check_map_semantics(t_Level *level)
 {
 	int		i;
-	int		j;
 
 	i = 0;
 	if (check_walls_top_bootom(level))
@@ -46,11 +53,6 @@ int	check_map_semantics(t_Level *level)
 	{
 		if (level->map[i][0] != '1' || level->map[i][level->limit->x] != '1')
 			return (ft_printf("Error\nThe map must be covered by walls\n"), 1);
-		j = 0;
-		while (level->map[i][j])
-		{
-			j++;
-		}
 		i++;
 	}
 	return (0);
